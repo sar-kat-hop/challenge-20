a5012a42e2df374505575e902369ecb117e8a0ad
Sarah <sarah.k.hopper@gmail.com>
2023-04-19 19:13:31 -0700

    add react-router code, not working

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,20 +1,34 @@
 import React from 'react';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomePg from './pages/HomePg';
+import PortfolioPg from './pages/PortfolioPg';
+import AboutPg from './pages/AboutPg';
 // import Project from './components/Project';
 
 function App() {
   return(
     <body>
         <Header />
-          <main>
+        <main>
             <div className='centered'>
-              {/* <Project /> */}
-              <h1 id='hello-1'>hello.</h1>
-              <p id='lead'> thank you for visiting my portfolio! </p>
-              <p id='instructions'> please click the links above to read more about me or take a look at some of my work. </p>
+            {/* <Project /> */}
+            <h1 id='hello-1'>hello.</h1>
+            <p id='lead'> thank you for visiting my portfolio! </p>
+            <p id='instructions'> please click the links above to read more about me or take a look at some of my work. </p>
             </div>
-          </main>
+        </main>
+
+        {/* <Router>
+          <Routes>
+            <Route path='/' element={HomePg} />
+            <Route path='about' element={AboutPg} />
+            <Route path='portfolio' element={PortfolioPg} />
+          </Routes>
+        </Router> */}
         <Footer />
       </body>
   )
--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,22 +1,46 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import AboutPg from '../pages/AboutPg';
+import About from '../components/About';
+import PortfolioPg from '../pages/PortfolioPg';
+import HomePg from '../pages/HomePg';
 
 function Header() {
     return(
+        <Router>
         <header className='main-header'>
             <h1 id='nav-head'>sarah <span id='kat'>k.</span> h.</h1>
             <nav>
                 <p>
-                    <a href="#about-me">about</a>
+                    <Link to='/'>home</Link>
                 </p>
                 <p>
-                    <a href="#flex">portfolio</a>
+                    {/* <a href="#about-me">about</a> */}
+                    <Link to='/about'>about</Link>
+                </p>
+                <p>
+                    {/* <a href="#flex">portfolio</a> */}
+                    <Link to='/portfolio'>portfolio</Link>
                 </p>
                 {/* may implement in future but currently just have contact info in footer */}
                 {/* <p>
-                    <a href="#contact-me" onClick={() => handlePageChange('Contact')}>contact</a>
+                    <a href="#contact-me">contact</a>
                 </p> */}
             </nav>
+
+            <Routes>
+                <Route path='/about' element={About}>
+                    {/* <AboutPg /> */}
+                </Route>
+                <Route path='/portfolio' element={PortfolioPg}>
+                    {/* <PortfolioPg /> */}
+                </Route>
+                <Route path='/' element={HomePg}>
+                    {/* <HomePg /> */}
+                </Route>
+            </Routes>
         </header>
+        </Router>
     )
 };
 
