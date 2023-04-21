d86432fb6ef487690a38dd4f1bbd54097a0518ca
Sarah <sarah.k.hopper@gmail.com>
2023-04-20 22:35:10 -0700

    add animations to svg icons, fade-ins for text, slide anims for header and footer

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 function Footer() {
     return(
-        <footer className='contact-me' id='contact-me'>
-            <h5>contact me</h5>
-            <p>
-                sarah.hopper042@gmail.com
-            </p>
+        <footer>
+            {/* <h5>contact me</h5> */}
+            <div className='footer-text'>
+                {/* <p id='connect'>
+                    connect with me on
+                </p> */}
+                {/* <p>
+                    sarah.hopper042@gmail.com
+                </p> */}
+
+            </div>
             <div className='icons' id="icons">
                 <div>
                     {/* github */}
@@ -16,6 +22,11 @@ function Footer() {
                     {/* linkedin */}
                     <a id="linkedin" href="https://www.linkedin.com/in/sarah-hopper-88437855/"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z"></path></svg></a>
                 </div>
+            </div>
+            <div>
+                <p id='made-by'>'
+                    made with <span id="heart">&#x2764;</span> by skh
+                </p>
             </div>
         </footer>
     )
--- portfolio/src/pages/HomePg.js
+++ portfolio/src/pages/HomePg.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 function HomePg() {
     return (
+        <body>
         <main>
             <div className='centered'>
-            <h1 id='hello-1'>hello.</h1>
-            <p id='lead'> thank you for visiting my portfolio! </p>
-            <p id='instructions'> please click the links above to read more about me or take a look at some of my work. </p>
+                <h1 id='hello-1'>hello.</h1>
+                    <p id='lead'> thanks for visiting! </p>
+                    <p id='instructions'> click the links above to read more about me or take a look at some of my work </p>
             </div>
         </main>
+        </body>
     )
 };
 
